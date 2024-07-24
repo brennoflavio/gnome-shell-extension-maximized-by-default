4094aebf9a8744579a13a57baf83715a465e6f3c
brennoflavio <contato@brennoflavio.com.br>
2024-07-24 01:47:23 -0300

    gnome guidelines

--- src/extension.js
+++ src/extension.js
@@ -1,9 +1,7 @@
 import Meta from 'gi://Meta';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-let _windowCreatedId;
-
-export default class ExampleExtension extends Extension {
+export default class MaximizedByDefaultExtension extends Extension {
     enable() {
         this._windowCreatedId = global.display.connect('window-created', (d, win) =>
             win.maximize(Meta.MaximizeFlags.HORIZONTAL | Meta.MaximizeFlags.VERTICAL)
